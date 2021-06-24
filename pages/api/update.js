94f51b6380b3215eaad7b680a67520b032dc7bf3
Ivan Dr <driga.ya27@gmail.com>
2021-06-25 02:06:50 +0300

    without editing files

--- pages/api/update.js
+++ pages/api/update.js
@@ -18,10 +18,10 @@ export default function updateHandler(req, res) {
 
     console.log('fileContent:', fileContent);
 
-    fs.writeFile(filePath, fileContent, (err) => {
-        if (err)
-            return res.status(500).json({ error: err.message });
-    });
+    // fs.writeFile(filePath, fileContent, (err) => {
+    //     if (err)
+    //         return res.status(500).json({ error: err.message });
+    // });
     // fs.writeFile(jsonPath, req.body, 'utf8', (err) => {
     //     if (err)
     //         return res.status(500).json({ error: err.message });
