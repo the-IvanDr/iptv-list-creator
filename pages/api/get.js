65e4a7c6b6898ef87f0bcc66949e4780b97d7680
Ivan Dr <driga.ya27@gmail.com>
2021-07-01 19:41:29 +0300

    move .json to public folder

--- pages/api/get.js
+++ pages/api/get.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default function getListHandler(req, res) {
-    const jsonPath = path.resolve(__dirname, '..', '..', '..', '..', 'src', 'iptvlist.json');
+    const jsonPath = path.resolve(__dirname, '..', '..', '..', '..', 'public', 'iptvlist.json');
     const list = JSON.parse(fs.readFileSync(jsonPath));
 
     console.log('list:', list);
--- pages/api/update.js
+++ pages/api/update.js
@@ -8,7 +8,7 @@ export default function updateHandler(req, res) {
     console.log('update -> list:', list);
 
     const filePath = path.resolve(__dirname, '..', '..', '..', '..', 'public', 'iptvlist.txt');
-    const jsonPath = path.resolve(__dirname, '..', '..', '..', '..', 'src', 'iptvlist.json');
+    const jsonPath = path.resolve(__dirname, '..', '..', '..', '..', 'public', 'iptvlist.json');
 
     let fileContent = '#EXTM3U\n';
     list.forEach(item => {
@@ -18,14 +18,14 @@ export default function updateHandler(req, res) {
 
     console.log('fileContent:', fileContent);
 
-    // fs.writeFile(filePath, fileContent, (err) => {
-    //     if (err)
-    //         return res.status(500).json({ error: err.message });
-    // });
-    // fs.writeFile(jsonPath, req.body, 'utf8', (err) => {
-    //     if (err)
-    //         return res.status(500).json({ error: err.message });
-    // });
+    fs.writeFile(filePath, fileContent, (err) => {
+        if (err)
+            return res.status(500).json({ error: err.message });
+    });
+    fs.writeFile(jsonPath, req.body, 'utf8', (err) => {
+        if (err)
+            return res.status(500).json({ error: err.message });
+    });
 
     return res.status(200).json({ success: true });
 }
