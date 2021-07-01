a2d53e12be8b9a3e5e252253f70c13da7cd8ade8
Ivan Dr <driga.ya27@gmail.com>
2021-07-01 20:16:13 +0300

    delete error handling during writefile()

--- pages/api/update.js
+++ pages/api/update.js
@@ -7,14 +7,14 @@ export default function updateHandler(req, res) {
 
 
 
-    
-    
-    
+
+
+
     console.log('update -> list:', list);
-    
+
     const filePath = path.resolve(__dirname, '..', '..', '..', '..', 'public', 'iptvlist.txt');
     const jsonPath = path.resolve(__dirname, '..', '..', '..', '..', 'public', 'iptvlist.json');
-    
+
     console.log("DIRECTORY: ***==============================================***");
 
     fs.readdir(path.resolve(__dirname, '..', '..', '..', '..', 'public'), (err, files) => {
@@ -24,7 +24,7 @@ export default function updateHandler(req, res) {
     });
 
     console.log("DIRECOTYR END: ***=========================================***");
-    
+
     let fileContent = '#EXTM3U\n';
     list.forEach(item => {
         fileContent += `#EXTINF:0, ${item.name}\n`;
@@ -34,13 +34,13 @@ export default function updateHandler(req, res) {
     console.log('fileContent:', fileContent);
 
     fs.writeFile(filePath, fileContent, (err) => {
-        if (err)
-            return res.status(500).json({ error: err.message, kek: 'lol, error is in writeFile() in .txt file' });
+        if (err) console.log('kek in .txt writeFile()')
+            // return res.status(500).json({ error: err.message });
     });
-    
+
     fs.writeFile(jsonPath, req.body, 'utf8', (err) => {
-        if (err)
-            return res.status(500).json({ error: err.message, kek: 'lol, one more error is in writeFile() in .json file' });
+        if (err) console.log('kek in .json writeFile()')
+            // return res.status(500).json({ error: err.message });
     });
 
     return res.status(200).json({ success: true });
