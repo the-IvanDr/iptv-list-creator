fa2d001ac30f52adff71653f86fa9f10c67668b2
Ivan Dr <driga.ya27@gmail.com>
2021-07-02 13:09:45 +0300

    return text

--- pages/api/getfile.js
+++ pages/api/getfile.js
@@ -11,11 +11,13 @@ export default async function getFile(req, res) {
         iptvTextContent += `EXTINF: 0, ${item.name}\n`;
         iptvTextContent += `${item.href}\n`;
     });   
-    
-    
-    // var text = `#EXTM3U\n#EXTINF: 0, kek\nhttp://data05-cdn.datalock.ru/gfi2lm/7e7690eac879d79887a76b3b3d77e8aa/grid/7f_01.Moy.muzhchina,.moya.zhenschina.Vodovorot.chuzhih.zhelaniy.s01.E01.2020.WEBRip.by.Nicodem.Files-x.a1.20.10.20.mp4`;
 
-    res.setHeader('Content-type', "application/octet-stream");
-    res.setHeader('Content-disposition', 'attachment; filename=file.m3u');
-    res.send(iptvTextContent);
+    // res.setHeader('Content-type', "application/octet-stream");
+    // res.setHeader('Content-disposition', 'attachment; filename=file.m3u');
+    // res.send(iptvTextContent);
+    
+    // res.setHeader('Content-type', "text/html");
+    // res.setHeader('Content-disposition', 'attachment; filename=file.m3u');
+    // res.send(iptvTextContent);
+    res.send(iptvTextContent)
 }
